d01045dc6859af4d9b86eb39d837f130ccf156f1
Artyom Dokuchaev <66699673+whitebelyash@users.noreply.github.com>
2026-06-21 01:18:50 +0400

    Feat[build]: add Android string onto runtime name
    Upstream SQLite checks for Android this way, thanks tomi

--- 6_buildjdk.sh
+++ 6_buildjdk.sh
@@ -126,6 +126,7 @@ bash ./configure \
     --with-cups-include=$CUPS_DIR \
     --with-devkit=$TOOLCHAIN \
     --with-native-debug-symbols=external \
+    --with-vendor-name="OpenJDK Runtime Environment (Android)" \
     --with-debug-level=$JDK_DEBUG_LEVEL \
     --with-fontconfig-include=$ANDROID_INCLUDE \
     $AUTOCONF_x11arg $AUTOCONF_EXTRA_ARGS \
